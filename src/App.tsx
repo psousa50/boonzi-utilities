113a7dc05a3f9f0e34f958a9718b8c2879a44f63
Pedro Sousa <psousa@equalexperts.com>
2019-04-26 15:04:40 +0100

    Improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { ChangeEvent } from "react"
 import "./App.css"
 import { processCreditCardBPI } from "./core/transforms/bpi"
+import { processEdenred } from "./core/transforms/edenred"
+const logoBPI = require("./images/logo-bpi.png")
+const logoEdenred = require("./images/logo-edenred.png")
 
 interface AppState {
   html: string
@@ -12,7 +15,7 @@ class App extends React.PureComponent<{}, AppState> {
 
   public render() {
     return (
-      <div className={"rowC"}>
+      <div className={"col"}>
         <div>
           <textarea
             style={{ width: "50%" }}
@@ -20,11 +23,25 @@ class App extends React.PureComponent<{}, AppState> {
             onPaste={this.onPaste}
             onChange={this.onChange}
             value={this.state.html}
+            onClick={this.clearInput}
+            placeholder={"paste table here..."}
           />
+        </div>
+        <div className={"row"}>
+          <button onClick={this.processBPI}>Copy</button>
+          <img width={100} src={logoBPI} alt="BPI" />
+        </div>
+        <div className={"row"}>
+          <button onClick={this.processEdenred}>Copy</button>
+          <img width={100} src={logoEdenred} alt="BPI" />
         </div>
         <div dangerouslySetInnerHTML={this.buildInnerHTMLTable()} />
       </div>
     )
+  }
+
+  private clearInput = () => {
+    this.updateInput("")
   }
 
   private buildInnerHTMLTable = () => ({
@@ -32,21 +49,35 @@ class App extends React.PureComponent<{}, AppState> {
   })
 
   private onPaste = (event: any) => {
-    this.updateResult(event.clipboardData.getData("text/html"))
+    this.updateInput(event.clipboardData.getData("text/html"))
   }
 
   private onChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    this.updateResult(event.target.value)
+    this.updateInput(event.target.value)
   }
 
-  private updateResult = (html: string) => {
+  private updateInput = (html: string) => {
+    this.setState(state => ({ ...state, html }))
+  }
+
+  private processBPI = () => {
+    const html = this.state.html
     const result = processCreditCardBPI(html)
+    this.setState(state => ({ ...state, result }))
 
     if (result) {
       navigator.clipboard.writeText(result)
     }
+  }
 
-    this.setState(state => ({ ...state, result, html }))
+  private processEdenred = () => {
+    const html = this.state.html
+    const result = processEdenred(html)
+    this.setState(state => ({ ...state, result }))
+
+    if (result) {
+      navigator.clipboard.writeText(result)
+    }
   }
 }
 
